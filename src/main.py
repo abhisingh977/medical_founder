422dabf2794834ab18d1d90dd48f1e271d89b538
Abhishek Singh <abhis1997singh@gmail.com>
2024-02-17 20:08:32 -0600

    1st

---
+++ src/main.py
@@ -0,0 +1,36 @@
+from flask import Flask, url_for, render_template, request, redirect, make_response, session
+from google.cloud import firestore
+from uuid import uuid1
+db = firestore.Client(project="healthstaffconnect")
+app = Flask("healthstaffconnect")
+
+session_uuid = str(uuid1())
+
+doctor_collection = db.collection("doctors")
+
+
+
+@app.route("/")
+def home():
+   return render_template("home.html")
+
+@app.route("/")
+def contact():
+   return render_template("home.html")
+
+
+@app.route('/add_doctor', methods=['POST'])
+def add_doctor():
+    if request.method == 'POST':
+        new_doctor = {
+            "name": request.form['name'],
+            "specialty": request.form['specialty'],
+            "location": request.form['location'],
+            "experience": request.form['experience'],
+        }
+        # doctors.append(new_doctor)
+        return redirect(url_for('home'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
