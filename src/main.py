7fe90a42c7a523261dff8d120f9cf63b99321ba2
Abhishek Singh <abhis1997singh@gmail.com>
2024-02-17 21:43:25 -0600

    python

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from flask import Flask, url_for, render_template, request, redirect, make_response, session
-from google.cloud import firestore
+# from google.cloud import firestore
 from uuid import uuid1
 # db = firestore.Client(project="healthstaffconnect")
 app = Flask("healthstaffconnect")
@@ -8,23 +8,24 @@ session_uuid = str(uuid1())
 
 # doctor_collection = db.collection("doctors")
 
-
-
 @app.route("/")
-def home():
+def index():
    return render_template("home.html")
 
-@app.route("/")
+@app.route("/contact")
 def contact():
    return render_template("home.html")
 
 
-@app.route('/add_doctor', methods=['POST'])
-def add_doctor():
-    if request.method == 'POST':
-        # doctors.append(new_doctor)
-        return redirect(url_for('home'))
+@app.route('/add_docter')
+def add_docter():
+    return render_template('add_doc.html')
+
+@app.route('/process_profile', methods=['POST'])
+def process_profile():
+    return render_template('add_doc.html')
+
 
 
 if __name__ == '__main__':
-    app.run(debug=False, port=8080)
+    app.run(debug=True, port=8080)
